94609c47f954b6a46e28fa2e63145c93c0710b65
Julian Affrime <julian.affrime@gmail.com>
2017-03-26 10:02:21 +0300

    server running, one user

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var app = express();
 var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(passport.initialize());
 
-
+//api routes
 app.get('/success', function(req, res) {
   res.send("Hey, hello from the server!");
 })
@@ -15,6 +16,21 @@ app.get('/success', function(req, res) {
 app.get('/login', function(req, res) {
   res.sendFile(__dirname + '/public/login.html');
 });
+
+app.post('/login', passport.authenticate('local', {
+  successRedirect: '/success',
+  failureRedirect: '/login',
+  session: false
+}));
+
+// hardcoded user for testing...
+passport.use(new LocalStrategy(function(username, password, done) {
+  if ((username === "John") && (password === "password")) {
+    return done(null, { username: username, id: 1 });
+  } else {
+    return done(null, false);
+  }
+}));
 
 
 app.listen(8000, function() {
